ecc4eb5cd46d74a330f4e996721d233d33c96071
Toba-dean <tobaogundimu@gmail.com>
2023-10-10 12:01:29 +0100

    add bottom nav with the google nav bar library, and used it to route between shop and cart pages

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,14 +1,43 @@
+import 'package:e_com/components/bottom_nav_bar.dart';
+import 'package:e_com/screens/cart_screen.dart';
+import 'package:e_com/screens/shop_screen.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  // this index controls the bottom nav bar
+  //0 == home and 1 == cart
+  int _selectedIdx = 0;
+
+  void navigateBottomBar(int index) {
+    setState(() {
+      _selectedIdx = index;
+    });
+  }
+
+  // Pages to navigate between
+  final List<Widget> _pages = [
+    // Shop Page
+    const ShopScreen(),
+
+    // Cart Page
+    const CartScreen()
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Home Page"),
+      backgroundColor: Colors.grey[300],
+      bottomNavigationBar: MyBottomNavBar(
+        onTabChange: (index) => navigateBottomBar(index),
       ),
+      body: _pages[_selectedIdx],
     );
   }
 }
